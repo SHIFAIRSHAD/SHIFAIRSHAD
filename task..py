69a52cf09bb1e3a03b2e4a6509e796a8b450a3fb
SHIFAIRSHAD <shifairshad36@gmail.com>
2025-04-19 17:30:58 -0700

    Add files via upload
    https://classroom.google.com/c/NzUwMjg0OTQ2MzM5

---
+++ task..py
@@ -0,0 +1,20 @@
+x = 13
+y = 12
+
+temp = x
+print("the value of temp variable is",temp)
+
+x = y 
+print ("the value of x is ", x)
+
+y = temp
+print ("the value of y is",y)
+
+
+
+x = 12
+y = 13
+
+x,y = y,x
+print("the value of x is", x)
+print("the value of y is", y)
